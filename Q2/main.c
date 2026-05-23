6e900ca02d105df59c88761180587a0df1288e7e
Felipe.Vram <fp562708@gmail.com>
2026-05-23 15:55:32 -0300

    modified:   Q2/main.c 	modified:   Q2/output/main.exe

--- Q2/main.c
+++ Q2/main.c
@@ -4,19 +4,30 @@ int main(){
 
     int alt, numeroimp;
 
-    printf("digite o valor da altura da pirâmide(o valor necessita ser maior ou igual a 2)\n");
+    printf("digite o valor da altura da piramide(o valor necessita ser maior ou igual a 2)\n");
     scanf("%d", &alt);
     
-    for (int i = 0; i < alt; i++)
+    for (int i = 0; i <= alt; i++)
     {
         numeroimp=1;
-        for (int j = 0; j < (alt*2)-1; j++)
+        for (int j = 0; j <= (alt*2)-1; j++)
         {
-            if (j>alt-(i-1) && j<alt+(i-1))
+            if (j>=alt-(i-1) && j<=alt)
             {
                 printf("%d\t", numeroimp);
                 numeroimp++;
+                if (j==alt)
+                {
+                    numeroimp--;
+                }
+                 
             }
+            else if (j>=alt && j<=alt+(i-1))
+            {
+                numeroimp--;
+                printf("%d\t", numeroimp);
+            }
+            
             else{
                 printf("\t");
             }
