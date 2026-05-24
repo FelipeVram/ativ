0ef05265f12b414718668a8f3b52fe3ecf01bd6b
Felipe.Vram <fp562708@gmail.com>
2026-05-24 18:52:09 -0300

    new file:   Q4/main.c 	new file:   Q4/output/main.exe

---
+++ Q4/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+    int larg;
+
+    printf("digite a largura central do losango\n");
+    scanf("%d", &larg);
+
+if (larg%2!=1)
+{
+    printf("tente novamente e digite um valor impar");
+}
+
+else{
+
+    for (int i = 1; i <= (larg+1)/2; i++)  //ESPAÇOS
+    {
+        for (int j = 1; j <= ((larg+1)/2)-i; j++)
+        {
+            printf(" ");
+        }
+        
+        for (int k = 1; k <= (2*i)-1; k++)
+        {
+            printf("X");
+        }
+        printf("\n");
+    }
+
+    for (int i = 1; i <= (larg-1)/2; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf(" ");
+        }
+    
+    for (int k = 1; k <= larg - (2*i); k++)
+    {
+        printf("X");
+    }
+        printf("\n");
+    }
+}
+return 0;
+
+}
