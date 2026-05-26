978e30b95d570464e963f6b0ab3f96e984c9d97d
Felipe.Vram <fp562708@gmail.com>
2026-05-26 15:36:00 -0300

    commit X

--- Q4/main.c
+++ Q4/main.c
@@ -6,41 +6,41 @@ int main(){
     printf("digite a largura central do losango\n");
     scanf("%d", &larg);
 
-if (larg%2!=1)
-{
-    printf("tente novamente e digite um valor impar");
-}
-
-else{
-
-    for (int i = 1; i <= (larg+1)/2; i++)  //ESPAÇOS
+    if (larg%2!=1)
     {
-        for (int j = 1; j <= ((larg+1)/2)-i; j++)
+        printf("tente novamente e digite um valor impar");
+    }
+
+    else{
+
+        for (int i = 1; i <= (larg+1)/2; i++)  
         {
-            printf(" ");
+            for (int j = 1; j <= ((larg+1)/2)-i; j++)
+            {
+                printf(" ");
+            }
+            
+            for (int k = 1; k <= (2*i)-1; k++)
+            {
+                printf("X");
+            }
+            printf("\n");
         }
+
+        for (int i = 1; i <= (larg-1)/2; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                printf(" ");
+            }
         
-        for (int k = 1; k <= (2*i)-1; k++)
+        for (int k = 1; k <= larg - (2*i); k++)
         {
             printf("X");
         }
-        printf("\n");
-    }
-
-    for (int i = 1; i <= (larg-1)/2; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            printf(" ");
+            printf("\n");
         }
-    
-    for (int k = 1; k <= larg - (2*i); k++)
-    {
-        printf("X");
     }
-        printf("\n");
-    }
-}
-return 0;
+    return 0;
 
-}
+    }
--- Q6/main.c
+++ Q6/main.c
@@ -56,4 +56,5 @@ int main()
             printf("\n");
         }
     }
+    return 0;
 }
